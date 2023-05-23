7e92eaaa2d4f86ea5bde40d5cbb3ed88023ac964
Saeed Mahmud <119888472+xsayedmahmud@users.noreply.github.com>
2023-05-23 14:03:47 +0300

    Add edit and delete functionality

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const modal = document.querySelector('.modal');
 const addBtn = document.querySelector('.add-btn');
 const closeModalBtn = document.querySelector('.close-modal-btn');
 const addItem = document.querySelector('.add-item');
+const bookForm = document.querySelector('.book-form');
+const getById = (id) => document.getElementById(id);
+const bookShelf = document.querySelector('.bookshelf');
+const myLibrary = [];
 
 addBtn.addEventListener('click', () => {
   modal.classList.add('active');
@@ -11,6 +15,8 @@ addBtn.addEventListener('click', () => {
 closeModalBtn.addEventListener('click', () => {
   modal.classList.remove('active');
   addItem.classList.remove('toggle');
+  bookForm.dataset.action = 'add';
+  delete bookForm.dataset.bookIndex;
 });
 
 class Book {
@@ -26,8 +32,20 @@ class Book {
 Book.prototype.published = undefined;
 Book.prototype.subject = undefined;
 
-const bookShelf = document.querySelector('.bookshelf');
-const myLibrary = [];
+const defaultBook = new Book(
+  '48 Laws of Power',
+  'Robert Green',
+  'English',
+  480,
+  false
+);
+
+defaultBook.published = 1998;
+defaultBook.subject = '#self-help';
+
+myLibrary.push(defaultBook);
+
+createBook(defaultBook);
 
 function createBook(book) {
   const newBook = document.createElement('div');
@@ -124,18 +142,14 @@ function createBook(book) {
   label.appendChild(span2);
 }
 
-const bookForm = document.querySelector('.book-form');
-
-bookForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const lang = document.getElementById('lang').value;
-  const pages = document.getElementById('pages').value;
-  const publishDate = document.getElementById('published').value;
-  const subject = document.getElementById('subject').value;
-  const status = document.getElementById('mark-read-form').checked;
+function addBook() {
+  const title = getById('title').value;
+  const author = getById('author').value;
+  const lang = getById('lang').value;
+  const pages = getById('pages').value;
+  const publishDate = getById('published').value;
+  const subject = getById('subject').value;
+  const status = getById('mark-read-form').checked;
 
   const book = new Book(title, author, lang, pages, status);
   book.published = publishDate;
@@ -149,4 +163,70 @@ bookForm.addEventListener('submit', (e) => {
   addItem.classList.remove('toggle');
 
   createBook(book);
+}
+
+function editBook() {
+  const { bookIndex } = bookForm.dataset;
+  const book = myLibrary[bookIndex];
+  book.title = getById('title').value;
+  book.author = getById('author').value;
+  book.lang = getById('lang').value;
+  book.pages = getById('pages').value;
+  book.publishDate = getById('published').value;
+  book.subject = getById('subject').value;
+  book.status = getById('mark-read-form').checked;
+
+  bookForm.reset();
+  modal.classList.remove('active');
+  addItem.classList.remove('toggle');
+
+  const bookDiv = document.querySelector(`.book[data-index = '${bookIndex}']`);
+
+  bookDiv.querySelector('.title').textContent = book.title;
+  bookDiv.querySelector('.author').textContent = `By ${book.author}`;
+  bookDiv.querySelector('.lang').textContent = `Language : ${book.lang} `;
+  bookDiv.querySelector('.pages').textContent = `Pages : ${book.pages}`;
+  bookDiv.querySelector(
+    '.published'
+  ).textContent = `Published : ${book.published}`;
+  bookDiv.querySelector('.subject').textContent = `Subject : ${book.subject}`;
+  bookDiv.querySelector('input[type="checkbox"]').checked = book.status;
+}
+
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (bookForm.dataset.action === 'add') {
+    addBook();
+  } else if (bookForm.dataset.action === 'edit') {
+    editBook();
+  }
+});
+
+bookShelf.addEventListener('click', (e) => {
+  if (e.target.matches('.close, .close *')) {
+    const book = e.target.closest('.book');
+    const { index } = book.dataset;
+    myLibrary.splice(index, 1);
+    book.remove();
+    bookForm.reset();
+  }
+
+  if (e.target.matches('.edit, .edit *')) {
+    const book = e.target.closest('.book');
+    const { index } = book.dataset;
+    const bookObj = myLibrary[index];
+    getById('title').value = bookObj.title;
+    getById('author').value = bookObj.author;
+    getById('lang').value = bookObj.lang;
+    getById('pages').value = bookObj.pages;
+    getById('published').value = bookObj.published;
+    getById('subject').value = bookObj.subject;
+    getById('mark-read-form').checked = bookObj.status;
+
+    bookForm.dataset.action = 'edit';
+    bookForm.dataset.bookIndex = index;
+
+    modal.classList.add('active');
+  }
 });
